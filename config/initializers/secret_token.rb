9056b695bb857bac2ade45eff613c509951168e6
svn <svn@ubidick.dyn.lan>
2012-02-07 12:54:56 +0100

    Initial first_app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '5eefa2bb4906b0b36e180cf3e7399ba62b91afa794bad4899bcd4069695a2e69d505f781765a8a0b56dd90d38a13e29f10028792047ddcd89b450b66eb5355c6'
